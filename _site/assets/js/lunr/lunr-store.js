8ec03e0801db2f14062c419d22bb86422333ce41
Bunlong <bunlong.van@gmail.com>
2018-05-14 10:54:30 +0700

    add Install Create React App

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"Create React App is a tool built by developers at Facebook to help you build React applications. It saves you from time-consuming setup and configuration. You simply run one command and create react app sets up the tools you need to start your React project.  ","categories": [],
         "tags": [],
         "url": "/",
+        "teaser":null},{
+        "title": "Installation",
+        "excerpt":"It is possible to manually create a React app, but Facebook has created a node module create-react-app to generate a boilerplate version of a React application. Besides providing something that works out-of-the-box, this has the added benefit of providing a consistent structure for React apps that you will recognize as...","categories": [],
+        "tags": [],
+        "url": "/install-create-react-app",
         "teaser":null},]
